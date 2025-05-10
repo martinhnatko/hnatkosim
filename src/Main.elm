777af1a963af07ec473ead95fe8fca4f0a79a971
martinhnatko <ccudny@gmail.com>
2025-05-10 18:27:03 +0200

    removed survey button

--- src/Main.elm
+++ src/Main.elm
@@ -451,44 +451,44 @@ view model =
                                 , class "px-1.5 text-sm sm:text-md sm:px-6 sm:py-3 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded shadow-lg transition-colors duration-200"
                                 ]
                                 [ text "Random Access Machine Simulator" ]
-                            , a
-                                [ href "https://docs.google.com/forms/d/e/1FAIpQLSdQoSvSqd-mCTB4yz9xMqjNfwCstgw_eWmdulsLwmxOKtusyA/viewform?usp=sf_link"
-                                , target "_blank"
-                                , rel "noopener noreferrer"
-                                , class 
-                                    """
-                                    px-1.5 text-sm sm:text-md
-                                    sm:px-6 sm:py-3 py-2
-                                    text-white 
-                                    font-bold 
+                            -- , a
+                            --     [ href "https://docs.google.com/forms/d/e/1FAIpQLSdQoSvSqd-mCTB4yz9xMqjNfwCstgw_eWmdulsLwmxOKtusyA/viewform?usp=sf_link"
+                            --     , target "_blank"
+                            --     , rel "noopener noreferrer"
+                            --     , class 
+                            --         """
+                            --         px-1.5 text-sm sm:text-md
+                            --         sm:px-6 sm:py-3 py-2
+                            --         text-white 
+                            --         font-bold 
                                     
-                                    rounded
-                                    shadow-xl
-                                    bg-gradient-to-r from-red-500 via-blue-500 to-orange-500 
+                            --         rounded
+                            --         shadow-xl
+                            --         bg-gradient-to-r from-red-500 via-blue-500 to-orange-500 
                                     
 
-                                    hover:animate-none
+                            --         hover:animate-none
                                     
-                                    flex items-center justify-center gap-2
+                            --         flex items-center justify-center gap-2
 
-                                    hover:bg-gradient-to-r hover:from-red-600 hover:via-blue-600 hover:to-orange-600
+                            --         hover:bg-gradient-to-r hover:from-red-600 hover:via-blue-600 hover:to-orange-600
                                     
-                                    twerk
-                                    """
-                                ]
-                                [ survey, text "SHORT FEEDBACK SURVEY" ]                            
+                            --         twerk
+                            --         """
+                            --     ]
+                            --     [ survey, text "SHORT FEEDBACK SURVEY" ]                            
 
-                                , p 
-                                    [ class """
-                                        text-xs 
-                                        text-gray-500 
-                                        text-center 
-                                        whitespace-normal 
-                                        break-words 
-                                        sm:max-w-[50ch]
-                                        max-w-[40ch]
-                                    """ ]
-                                    [ text "*Please take a moment to fill out this short survey, it's part of my bachelor's thesis." ]
+                            --     , p 
+                            --         [ class """
+                            --             text-xs 
+                            --             text-gray-500 
+                            --             text-center 
+                            --             whitespace-normal 
+                            --             break-words 
+                            --             sm:max-w-[50ch]
+                            --             max-w-[40ch]
+                            --         """ ]
+                            --         [ text "*Please take a moment to fill out this short survey, it's part of my bachelor's thesis." ]
                             ]
                         ]
 
@@ -504,7 +504,7 @@ view model =
                                 ]
                                 [ heroiconInfo, text "About" ]
                             , a
-                                [ href "https://docs.google.com/forms/d/e/1FAIpQLSe4agj2hvxDIDLTRTOW5YYO4QNrZwihH4uf5q9CBlslGeUrAg/viewform?usp=dialog"
+                                [ href "https://github.com/martinhnatko/hnatkosim/issues"
                                 , target "_blank"
                                 , rel "noopener noreferrer"
                                 , class "sm:w-1/3 px-3 py-1.5 font-semibold rounded shadow-lg transition-colors duration-200 border border-blue-500 text-blue-500 bg-white hover:bg-blue-50  focus:outline-none flex items-center justify-center gap-1"
